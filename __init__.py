bc5ef56235ce1442abccd30b2d31a32b7525d350
Carlos Paredes <cparedes@covetel.com.ve>
2014-06-04 11:34:14 -0430

    Añadiendo archivos de inicialización del módulo OpenERP
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+import report_helper
+import orgmode_report
---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Org-mode Report Engine',
+    'description': """
+This module adds a new Report Engine based on Emacs Org-mode to support reports designed in Org-mode + Latex.
+=====================================================================================================================
+
+Requirements and Installation:
+------------------------------
+This module requires ``emacs`` and ``pystache`` library to render Org-mode documents as
+PDF.""",
+
+    'version': '0.1',
+    'depends': ['base'],
+    'author': 'Covetel R.S',
+    'category': 'Reporting',
+    'url': 'http://www.covetel.com.ve/',
+    'data': [ 
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
